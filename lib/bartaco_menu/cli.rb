5f2f9feb2e8b56f0db552efe834587c575b9db3b
danny galimi <dannygalimi13@aim.com>
2021-03-12 19:11:24 -0500

    more method work

--- lib/bartaco_menu/cli.rb
+++ lib/bartaco_menu/cli.rb
@@ -1,14 +1,27 @@
 class BartacoMenu::CLI
     def call
         puts "Welcome to Bartaco"
+        get_menu_items
+        get_user_item
+        # get_menu_items
         # get_user_item
         # get_info_for_item
         #list_item_info
     end 
 
+    def get_menu_items
+        # to be scrapped instead
+        @items = ['duck', 'chicken', 'chorizo', 'falafel']
+    end
+
     def get_user_item
-        item = []
-    end 
+        # list items
+        @items.each_with_index do  |item, index|
+          puts "#{index} #{item}"
+        end
+    end
+    
+end 
 
 
 end  
