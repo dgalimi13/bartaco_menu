60291c725df5ed97e6ab5c792159eafe99124d2d
danny galimi <dannygalimi13@aim.com>
2022-06-05 22:50:36 -0400

    added initialize and self.all methods to Allergy class

--- lib/bartaco_menu/allergy.rb
+++ lib/bartaco_menu/allergy.rb
@@ -1,3 +1,20 @@
 class Allergy
 
+    attr_accessor :name, :ingredients
+    @@all = []
+
+    def initialize(name)
+    @name = name 
+    @ingredients = []
+    save
+    end
+
+    def self.all
+        BartacoMenu::Scraper.scrape_items if @@all.empty?
+        @@all
+        
+    end
+
+    
+
 end 
